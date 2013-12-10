b14b92870a06dc2ed44cdfb125f4f7cbd65e58c2
André Aubin <andre.aubin@lambdaweb.fr>
2013-12-10 08:45:41 +0100

    Adding new js, css and less files, including Gruntfile, package.json and Banner (Banner is the muffin banner :)

--- Templates/Drafter/src/js/main.js
+++ Templates/Drafter/src/js/main.js
@@ -2,30 +2,29 @@
 
 $(document).ready(function()
 {
-	initalizeForm();
-	reloadHandlers();
-	$.bind("ajaxComplete", function() {
-		reloadHandlers();
+	var dr = $("#draft-aera");
+	var mk = $("#marked-aera");
+
+	dr.change(function()
+	{
+		var ctnt = $(this).val();
+		var mkd = marked(ctnt);
+		mk.html(mkd);
+	});
+	
+	$('[data-action="preview"]').click(function()
+	{
+		dr.hide();
+		mk.show();
+		$('[data-action="edit"]').show();
+		$('[data-action="preview"]').hide();
 	});
 
-	/**
-	 * Json calls to Github Api
-	 * @type @exp;$@call;getJSON
-	 */
-	$.getJSON("https://api.github.com/repos/lambda2/Muffin/tags", function(dataT)
+	$('[data-action="edit"]').click(function()
 	{
-		console.log(dataT);
-		var lastSha = dataT[0].commit.sha;
-		$.getJSON("https://api.github.com/repos/lambda2/Muffin/commits/" + lastSha, function(dataC)
-		{
-			var lastCommit = dataC;
-			var cDate = lastCommit.commit.committer.date.slice(0, 10);
-			var cAdd = lastCommit.stats.additions;
-			var cDel = lastCommit.stats.deletions;
-			var cStats = "<span class='icon-flow-tree'></span> <span class='git-add'>+" + cAdd + "</span>  <span class='git-del'>-" + cDel + "</span>";
-			$("div[role='git-info']").html("<p>\n\
-				<a href='https://github.com/lambda2/Muffin'><span class='icon-github'></span>\n\
-				v" + dataT[0].name + " datant du " + cDate + "  ─  " + cStats + "</a> </p>");
-		});
+		dr.show();
+		mk.hide();
+		$('[data-action="edit"]').hide();
+		$('[data-action="preview"]').show();
 	});
 });
